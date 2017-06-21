c7e25c5c27e8abd635dfa3dade18a7c4962a8179
chesteraustin <chesteraustin@gmail.com>
2017-06-20 23:19:36 -0700

    scaffold liri actions

---
+++ liri.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function(){
+	var key = require("./key");
+	var songs = require("./songs");
+
+	var command = process.argv[2];
+	var term = process.argv[3];
+
+	switch(command) {
+		case "my-tweets":
+			console.log("my-tweets");
+			break;
+		case "spotify-this-song":
+			console.log("spotify-this-song");
+			break;
+		case "movie-this":
+			console.log("movie-this");
+			break;
+		case "do-what-it-says":
+			console.log("do-what-it-says");
+			break;
+		default:
+			console.log("default");
+	}
+})();
