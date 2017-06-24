5b8d82a1c7307d7743a84b637c55fbf403f01a7a
chesteraustin <chesteraustin@gmail.com>
2017-06-23 22:18:38 -0700

    create directions for default mode

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@
 	var tweets = require("./tweets")
 	var songs = require("./songs");
 	var movies = require("./movies");
-	var random = require("./random");;
+	var random = require("./random");
 
 	var command = process.argv[2];
 	var term = process.argv[3];
@@ -23,6 +23,6 @@
 			random.randomAction();
 			break;
 		default:
-			console.log("default");
+			console.log("Please select a command `my-tweets` or `spotify-this-song \"SONG-TITLE\"` or `movie-this \"MOVIE-TITLE\"` or `do-what-it-says`");
 	}
 })();
